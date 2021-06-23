28bbb5b62644fe2ededf6bce97d8ce070baeaf3f
hakan <hakancoskun360@gmail.com>
2021-06-23 20:55:33 +0300

    ilk push

---
+++ hrmsfe/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Container className="main">
+        <h1>merhaba sitem benim</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
